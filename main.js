005b17db42f5d999656f9d8b1ebe63d147cbebb5
alfinarif <cleavercode@gmail.com>
2024-12-23 01:44:47 +0300

    GET request practice

--- main.js
+++ main.js
@@ -2,13 +2,27 @@
 const http = require("http");
 
 const server = http.createServer(function(req, res){
-    res.end("Hello Word From NodeJs");
+
+    // get request
+    if (req.url == "/"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h1>This is Home Page</h1>");
+        res.end();
+    }
+    else if(req.url == "/about"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h1>This is About Page</h1>");
+        res.end();
+    }
+    else if(req.url == "/contact"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write("<h1>This is Contact Page</h1>");
+        res.end();
+    }
+
 });
 
 server.listen(5050);
-console.log("Server run success");
 
-
-
-
+console.log("Server Started Successfully!");
 
