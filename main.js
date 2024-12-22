ea08872d52706321199fe0fbc1fc9cf50b92a959
alfinarif <cleavercode@gmail.com>
2024-12-23 02:21:14 +0300

    created package.json file

--- main.js
+++ main.js
@@ -3,26 +3,23 @@ const http = require("http");
 
 const server = http.createServer(function(req, res){
 
-    // get request
     if (req.url == "/"){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("<h1>This is Home Page</h1>");
+        res.writeHead(200, {"Content-Type": "Alfin Arif"});
+        res.write("<h1>This is Home Page From NodeJs Server!</h1>");
         res.end();
     }
-    else if(req.url == "/about"){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("<h1>This is About Page</h1>");
+    else if (req.url == "/about"){
+        res.writeHead(200, {"Content-Type": "Alfin Arif"});
+        res.write("<h1>This is About Page From NodeJs Server!</h1>");
         res.end();
     }
-    else if(req.url == "/contact"){
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write("<h1>This is Contact Page</h1>");
+    else if (req.url == "/contact"){
+        res.writeHead(200, {"Content-Type": "Alfin Arif"});
+        res.write("<h1>This is Contact Page From NodeJs Server!</h1>");
         res.end();
     }
 
 });
 
 server.listen(5050);
-
-console.log("Server Started Successfully!");
-
+console.log("Server Started...");
